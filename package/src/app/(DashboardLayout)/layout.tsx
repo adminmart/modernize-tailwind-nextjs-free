8ae0b9f8f2066705bdc7fa94061f2ce65f513542
wrappixelTeam <wrappixelteam.2016@gmail.com>
2025-11-07 12:05:07 +0530

    topbar added

--- package/src/app/(DashboardLayout)/layout.tsx
+++ package/src/app/(DashboardLayout)/layout.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import Header from './layout/header/Header'
+import Topbar from './layout/header/Topbar'
 import Sidebar from './layout/sidebar/Sidebar'
 
 export default function Layout({
@@ -9,19 +10,22 @@ export default function Layout({
   children: React.ReactNode
 }>) {
   return (
-    <div className='flex w-full min-h-screen'>
-      <div className='page-wrapper flex w-full'>
-        {/* Header/sidebar */}
-        <div className='xl:block hidden'>
-          <Sidebar />
-        </div>
-        <div className='body-wrapper w-full bg-background'>
-          {/* Top Header  */}
-          <Header />
-          {/* Body Content  */}
-          <div className={`container mx-auto px-6 py-30`}>{children}</div>
+    <>
+      <Topbar />
+      <div className='flex w-full min-h-screen'>
+        <div className='page-wrapper flex w-full'>
+          {/* Header/sidebar */}
+          <div className='xl:block hidden'>
+            <Sidebar />
+          </div>
+          <div className='body-wrapper w-full bg-background'>
+            {/* Top Header  */}
+            <Header />
+            {/* Body Content  */}
+            <div className={`container mx-auto px-6 py-30`}>{children}</div>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   )
 }
